043623bd9a9494a95e438f17fcb78bfba1231fa3
Craig Martin <craig.martin@accedo.tv>
2023-05-28 13:56:43 +1000

    Revert "Merge pull request #4 from craig-martin-accedo/feature"
    This reverts commit a15aabaa355ced3e1fc82656b8df2a112832a2e7, reversing
    changes made to 4df99ca1bc8278874dea9ac894b131010bb60cfc.

--- NASATests/NASATests.swift
+++ NASATests/NASATests.swift
@@ -21,7 +21,7 @@ final class NASATests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let i = 1
+        let i = 0
         
         XCTAssert(i == 0, "I is supposed to be 0")
     }
